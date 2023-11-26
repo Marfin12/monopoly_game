82d01d304fe666b07b4183f1622db91ad29eadb6
Marfin Fadhilah <marfin.fadhilah@bankneo.co.id>
2023-11-26 21:32:29 +0700

    - start menu (cities & cloud & 2 buttons background & google logo)

--- app/src/main/java/com/example/experiments2/component/button/GameNormalButton.kt
+++ app/src/main/java/com/example/experiments2/component/button/GameNormalButton.kt
@@ -29,23 +29,54 @@ open class GameNormalButton @JvmOverloads constructor(
             val txtButton = findViewById<TextView>(R.id.tv_normal_text)
             val txtSymbol = findViewById<TextView>(R.id.tv_symbol)
             val txtWithSymbol = findViewById<TextView>(R.id.tv_with_symbol)
-            val ivButton = findViewById<ImageView>(R.id.holder_bg_button)
+            val ivSymbol = findViewById<ImageView>(R.id.iv_symbol)
+            val ivBackground = findViewById<ImageView>(R.id.holder_bg_button)
 
-            generateSymbol(ta, txtButton, txtSymbol, txtWithSymbol)
+            generateSymbol(ta, txtButton, txtSymbol, txtWithSymbol, ivSymbol)
             generateButton(ta, txtButton)
-
+            generateButton(ta, txtWithSymbol)
+            generateColor(ta, txtButton, txtSymbol, ivBackground)
         } finally {
             ta.recycle()
         }
+    }
+
+    private fun generateColor(
+        ta: TypedArray,
+        txtButton: TextView,
+        txtWithSymbol: TextView,
+        ivBackground: ImageView
+    ) {
+        txtButton.setTextColor(
+            resources.getColor(
+                ta.getResourceId(R.styleable.GameButton_textColor, R.color.black),
+                context.theme
+            )
+        )
+        txtWithSymbol.setTextColor(
+            resources.getColor(
+                ta.getResourceId(R.styleable.GameButton_textColor, R.color.black),
+                context.theme
+            )
+        )
+        ivBackground.setImageResource(ta.getResourceId(
+            R.styleable.GameButton_buttonColor,
+            R.drawable.white_rounded_bg_8_rad
+        ))
     }
 
     private fun generateSymbol(
         ta: TypedArray,
         txtButton: TextView,
         txtSymbol: TextView,
-        txtWithSymbol: TextView
+        txtWithSymbol: TextView,
+        ivSymbol: ImageView
     ) {
         val symbol = ta.getString(R.styleable.GameButton_symbol)
+        val symbolLogo = ta.getResourceId(
+            R.styleable.GameButton_symbol_logo,
+            0
+        )
         val symbolTop = ta.getInteger(R.styleable.GameButton_symbol_top, 0)
         val symbolBottom = ta.getInteger(R.styleable.GameButton_symbol_bottom, 0)
         val symbolEnd = ta.getInteger(R.styleable.GameButton_symbol_end, 0)
@@ -53,12 +84,13 @@ open class GameNormalButton @JvmOverloads constructor(
 
         val text = ta.getString(R.styleable.GameButton_text)
 
-        if (symbol.isNullOrBlank()) {
+        if (symbol.isNullOrBlank() && symbolLogo == 0) {
             txtButton.text = text
             txtButton.visibility = View.VISIBLE
             txtSymbol.visibility = View.GONE
             txtWithSymbol.visibility = View.GONE
-        } else {
+        }
+        else if(!symbol.isNullOrBlank()) {
             txtButton.visibility = View.INVISIBLE
             txtWithSymbol.visibility = View.VISIBLE
             txtSymbol.visibility = View.VISIBLE
@@ -69,6 +101,16 @@ open class GameNormalButton @JvmOverloads constructor(
             txtButton.text = text
             txtSymbol.y += (symbolTop - symbolBottom)
             txtSymbol.x += (symbolStart - symbolEnd)
+        }
+        else if(symbolLogo != 0) {
+            txtButton.visibility = View.INVISIBLE
+            txtWithSymbol.visibility = View.VISIBLE
+            ivSymbol.visibility = View.VISIBLE
+
+            txtWithSymbol.text = text
+            txtButton.text = text
+
+            ivSymbol.setImageResource(symbolLogo)
         }
     }
 
